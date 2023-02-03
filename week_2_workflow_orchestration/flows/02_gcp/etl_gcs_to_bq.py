58e6a5d5963e1aaf7950e74fd13d04eb6f541734
antihaddock <antihaddock1@gmail.com>
2023-02-03 17:40:47 +1100

    Adding files for Week 2 post homework

--- week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
@@ -9,7 +9,7 @@ from prefect_gcp import GcpCredentials
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
-    gcs_block = GcsBucket.load("zoom-gcs")
+    gcs_block = GcsBucket.load("data-engineering-zoomcamp")
     gcs_block.get_directory(from_path=gcs_path, local_path=f"../data/")
     return Path(f"../data/{gcs_path}")
 
@@ -19,37 +19,45 @@ def transform(path: Path) -> pd.DataFrame:
     """Data cleaning example"""
     df = pd.read_parquet(path)
     print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
-    df["passenger_count"].fillna(0, inplace=True)
+   # df["passenger_count"].fillna(0, inplace=True)
     print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
     return df
 
 
-@task()
+@task(log_prints=True)
 def write_bq(df: pd.DataFrame) -> None:
-    """Write DataFrame to BiqQuery"""
-
-    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+    """Write DataFrame to BigQuery"""
+    gcp_credentials_block = GcpCredentials.load("data-engineering-creds")
 
     df.to_gbq(
-        destination_table="dezoomcamp.rides",
-        project_id="prefect-sbx-community-eng",
+        destination_table="datazoomcamp.green_rides",
+        project_id="datazoomcap2023",
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
         if_exists="append",
     )
+    print(f'n rows = {len(df)}')
 
 
 @flow()
-def etl_gcs_to_bq():
+def etl_gcs_to_bq(year: int, month: int, color: str) -> None:
     """Main ETL flow to load data into Big Query"""
-    color = "yellow"
-    year = 2021
-    month = 1
+    color = color
+    year = year
+    month = month
 
     path = extract_from_gcs(color, year, month)
     df = transform(path)
     write_bq(df)
 
+@flow()
+def etl_parent_flow(months: list[int], year: int, color: str):
+    for month in months:
+        etl_gcs_to_bq(year, month, color)
+
 
 if __name__ == "__main__":
-    etl_gcs_to_bq()
+    color = "green"
+    months = [ 4]
+    year = 2019
+    etl_parent_flow(months, year, color)
--- week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
@@ -5,12 +5,10 @@ from prefect_gcp.cloud_storage import GcsBucket
 from random import randint
 
 
+
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
-    # if randint(0, 1) > 0:
-    #     raise Exception
-
     df = pd.read_csv(dataset_url)
     return df
 
@@ -18,8 +16,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -37,17 +35,18 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
 @task()
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.upload_from_path(from_path=path, to_path=path)
+    
+    gcs_block = GcsBucket.load("data-engineering-zoomcamp")
+    gcs_block.upload_from_path(from_path=path, to_path=path, timeout=None)
     return
 
 
 @flow()
 def etl_web_to_gcs() -> None:
     """The main ETL function"""
-    color = "yellow"
-    year = 2021
-    month = 1
+    color = "green"
+    year = 2019
+    month = 4
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
@@ -55,7 +54,6 @@ def etl_web_to_gcs() -> None:
     df_clean = clean(df)
     path = write_local(df_clean, color, dataset_file)
     write_gcs(path)
-
 
 if __name__ == "__main__":
     etl_web_to_gcs()
